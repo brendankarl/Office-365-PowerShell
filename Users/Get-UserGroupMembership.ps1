bd1c0f9440d565351411dcb366eaa63ee20dddb7
Brendan Griffin <brendankgriffin@hotmail.com>
2015-06-02 13:45:57 +0100

    Added Get-UserGroupMembership
    Can be used to obtain the Azure AD Groups that a specific user is a
    member of.

---
+++ Users/Get-UserGroupMembership.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module MSONline
+#Obtain Credentials and Authenticate to Azure AD
+$Username = Read-Host -Prompt "Enter your username"
+$Password = Read-Host -Prompt "Enter your password" -AsSecureString
+$Cred = New-Object System.Management.Automation.PSCredential $Username, $Password
+Connect-MsolService -Credential $Cred
+
+$User = Read-Host -Prompt "Enter user e-mail address of user to obtain group membership for"
+Write-Host "$User is a member of the following groups:" -ForegroundColor Green 
+Foreach ($MSOLGroup in (Get-MsolGroup))
+{
+If (Get-MsolGroupMember -GroupObjectId $MSOLGroup.ObjectId | Where {$_.EmailAddress -eq $User})
+    {
+    Write-Host "-" $MSOLGroup.DisplayName -ForegroundColor Yellow
+    }
+}
