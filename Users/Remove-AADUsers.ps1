63f8c740313db31c6066499f951776f9494b736a
Brendan Griffin <brendankgriffin@hotmail.com>
2015-06-03 13:39:49 +0100

    Added Remove-AADUsers
    Can be used to perform a bulk deletion of users whose UPN matches a
    specific criteria.

---
+++ Users/Remove-AADUsers.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module MSONline
+#Obtain Credentials and Authenticate to Azure AD
+$Username = Read-Host -Prompt "Enter your username"
+$Password = Read-Host -Prompt "Enter your password" -AsSecureString
+$Cred = New-Object System.Management.Automation.PSCredential $Username, $Password
+Connect-MsolService -Credential $Cred
+
+#Update "user*" with the value you require, all users matching this criteria will be deleted from Azure AD
+
+$Users = Get-MsolUser | where {$_.UserPrincipalName -like "user*"}
+$Users | ForEach-Object {Remove-MsolUser -UserPrincipalName $_.userprincipalname -Force}
