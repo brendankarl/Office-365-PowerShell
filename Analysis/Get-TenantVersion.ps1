3de42294f516752b663391ae5121473695539e7f
Brendan Griffin <brendankgriffin@hotmail.com>
2016-01-07 14:12:53 +0000

    Added Get-SPTenantVersion
    Reports the version of an SPO tenant

---
+++ Analysis/Get-TenantVersion.ps1
@@ -0,0 +1,16 @@
+﻿#Add references to SharePoint client assemblies and authenticate to Office 365 site
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+
+#Obtain username and password
+$Username = Read-Host -Prompt "Please enter your username"
+$Password = Read-Host -Prompt "Please enter your password" -AsSecureString
+
+#Connect to root Site Collection snd obtain version
+$Site = "https://tenant.sharepoint.com"
+$Context = New-Object Microsoft.SharePoint.Client.ClientContext($Site)
+$Creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username,$Password)
+$Context.Credentials = $Creds
+$Context.ExecuteQuery()
+
+Write-Host "Server Version:" $Context.ServerVersion -ForegroundColor Green
