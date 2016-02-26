7da02c195a402e2436850a1a1f7cd7a526f54769
Brendan Griffin <brendankgriffin@hotmail.com>
2016-02-26 10:58:46 +0000

    Added Get-SiteUserPermissions
    Provides details of all users who have permission to a Site Collection.

---
+++ Users/Get-SiteUserPermissions.ps1
@@ -0,0 +1,10 @@
+﻿#Specify Tenant Admin URL
+$TenantAdminURL = "https://tenant-admin.sharepoint.com"
+$SiteURL = "https://tenant.sharepoint.com/sites/site"
+
+#O365 Cmdlets to connect to a tenant
+Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking -ErrorAction SilentlyContinue
+Connect-SPOService -Url $TenantAdminURL
+
+#Return all users for a specific Site Collection
+Get-SPOUser -Site $SiteURL | Select DisplayName, LoginName, IsSiteAdmin, Groups | ft -AutoSize                                                                                           
