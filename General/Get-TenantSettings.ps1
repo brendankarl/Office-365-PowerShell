d77877b24f8ef81a87a8f990a4daa62b01e7b279
Brendan Griffin <brendankgriffin@hotmail.com>
2016-06-01 14:02:34 +0100

    Fixed typo in Get-TenantSettingd

--- General/Get-TenantSettings.ps1
+++ General/Get-TenantSettings.ps1
@@ -2,7 +2,7 @@
 $Site = "https://tenant-admin.sharepoint.com"
 
 #Add references to SharePoint client assemblies and authenticate to Office 365 site
-Add-Type -Path "D:\CSOM\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "D:\CSOM\Microsoft.SharePoint.Client.dll"
 Add-Type -Path "D:\CSOM\Microsoft.SharePoint.Client.Publishing.dll"
 Add-Type -Path "D:\CSOM\Microsoft.Online.SharePoint.Client.Tenant.dll"
 $Username = Read-Host "Please enter your username"
