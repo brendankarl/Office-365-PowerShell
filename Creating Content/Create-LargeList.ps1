8614c5cb7946eed351a0259aa09dbffcb2139f66
Brendan Griffin <brendankgriffin@hotmail.com>
2016-07-05 12:40:13 +0100

    Added Create and Update-LargeList
    Can be used to quickly create a list with a large number of items, the update script can be used to perform bulk updates across all items.

---
+++ Creating Content/Create-LargeList.ps1
@@ -0,0 +1,25 @@
+﻿#Add references to SharePoint client assemblies and authenticate to Office 365 site
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+$Username = Read-Host -Prompt "Please enter your username"
+$Password = Read-Host -Prompt "Please enter your password" -AsSecureString
+$Site = "https://tenant.sharepoint.com"
+$Context = New-Object Microsoft.SharePoint.Client.ClientContext($Site)
+$Creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username,$Password)
+$Context.Credentials = $Creds
+
+$List = $Context.Web.Lists.GetByTitle("CustomList")
+$Context.Load($List)
+$Context.ExecuteQuery()
+
+$i = 0
+While ($i -lt "500")
+{
+$ListItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$Item = $List.AddItem($ListItemInfo)
+$Item["Title"] = "Title$i"
+$Item["Product"] = "Product$i"
+$Item.Update()
+$Context.ExecuteQuery() 
+$i++
+}
---
+++ Creating Content/Update-LargeList.ps1
@@ -0,0 +1,30 @@
+﻿#Add references to SharePoint client assemblies and authenticate to Office 365 site
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+$Username = Read-Host -Prompt "Please enter your username"
+$Password = Read-Host -Prompt "Please enter your password" -AsSecureString
+$Site = "https://tenant.sharepoint.com"
+$Context = New-Object Microsoft.SharePoint.Client.ClientContext($Site)
+$Creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username,$Password)
+$Context.Credentials = $Creds
+
+$List = $Context.Web.Lists.GetByTitle("CustomList")
+$Context.Load($List)
+$Context.ExecuteQuery()
+
+$ListItemsCAML = New-Object Microsoft.SharePoint.Client.CamlQuery
+$ListItemsCAML.ViewXml = "<View Scope='RecursiveAll'></View>"
+$ListItems = $List.GetItems($ListItemsCAML)
+$Context.Load($ListItems)
+$Context.ExecuteQuery()
+
+$i = 2000
+Foreach ($ListItem in $ListItems)
+{
+
+$ListItem["Title"] = "TitleUpdate$i"
+$ListItem["Product"] = "ProductUpdate$i"
+$ListItem.Update()
+$Context.ExecuteQuery() 
+$i++
+}
